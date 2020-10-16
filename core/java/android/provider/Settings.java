d05487e0835d0cc592ddf550804f931b849f6f6c
00day0 <therandomuser11@gmail.com>
2020-10-16 10:28:12 +0000

    QS Header data usage info [1/2]
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I446d9b8a0bb7decaec19f44b54d0a421c916a8d1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4972,6 +4972,12 @@ public final class Settings {
          */
         public static final String QS_MEDIA_PLAYER = "qs_media_player";
 
+	/**
+         * Show data usage in QS header
+         * @hide
+         */
+        public static final String QS_DATAUSAGE = "qs_datausage";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
